06ae129083ee9b4f7669cd4392b9392138112003
Giga <60932604+Gkhundadze@users.noreply.github.com>
2020-11-23 17:23:04 -0800

    made some updates
    added  alert when input is clear and clicked on submit btn

--- script.js
+++ script.js
@@ -9,48 +9,62 @@
   const checkbox = document.createElement('input');
   const receiveData = 'https://us-central1-js04-b4877.cloudfunctions.net/tasks';
   const addData = "https://us-central1-js04-b4877.cloudfunctions.net/tasks/create";
-  
-  // STYLING
-  ul.style.listStyleType = "none"
-  deleteBtn.classList.add("btn", "btn-secondary", "btn-sm");
-  
-  checkData(receiveData);
-  form.addEventListener("submit",(fun) => {
-    fun.preventDefault();
-    let inputValue = input.value;
-    createTask(inputValue);
-    checkData(receiveData);
-  });
-  
-  
-  
-  function checkData(url){
-    fetch(receiveData,{
-    method: "GET"
-  })
-  .then(response => response.json())
-  .then(data => {
-    let taskList = data.data
-    taskList.forEach(task => {
-      const listItem = document.createElement('li');
-      const listText = document.createElement('span');
-      const deleteBtn = document.createElement('button');
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-        listItem.appendChild(checkbox);
-        listItem.appendChild(listText);
-        listText.textContent = task.text;
-        listItem.appendChild(deleteBtn);
-        deleteBtn.textContent = 'Delete';
-        ul.appendChild(listItem);
-        
-        // STYLING
-        ul.style.listStyleType = "none"
-        deleteBtn.classList.add("btn", "btn-secondary", "btn-sm")
 
-    });
+  
+checkData(receiveData);
+
+form.addEventListener("submit",(fun) => {
+  fun.preventDefault();
+  let inputValue = input.value;
+  if(!inputValue == ""){
+    createTask(inputValue);
+  }else{
+    alert("შეიყვანეთ დასახელება")
+  }
+  
+});
+
+  
+  
+  
+
+
+
+
+
+
+
+
+                                // F U N C T I O N S
+
+// receive Data from API and show in HTML
+function checkData(url){
+  fetch(receiveData,{
+  method: "GET"
+})
+.then(response => response.json())
+.then(data => {
+  let taskList = data.data
+  taskList.forEach(task => {
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const deleteBtn = document.createElement('button');
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+      listItem.appendChild(checkbox);
+      listItem.appendChild(listText);
+      listText.textContent = task.text;
+      listItem.appendChild(deleteBtn);
+      deleteBtn.textContent = 'Delete';
+      ul.appendChild(listItem);
+      
+      // STYLING
+      ul.style.listStyleType = "none"
+      deleteBtn.classList.add("btn", "btn-secondary", "btn-sm")
   });
-  };
+});
+};
+  // AddTask to API
   function createTask(inputValue){
     fetch(addData,{
     method: "POST",
@@ -66,18 +80,3 @@
     dataToHTM(data);
   });
 }
-function dataToHTML(text){
-  taskList.forEach(task => {
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const deleteBtn = document.createElement('button');
-    const checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-      listItem.appendChild(checkbox);
-      listItem.appendChild(listText);
-      listText.textContent = task.text;
-      listItem.appendChild(deleteBtn);
-      deleteBtn.textContent = 'Delete';
-      ul.appendChild(listItem);
-  });
-};
