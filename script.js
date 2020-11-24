150b08d30cd2e1cd891684db30a63c7a1d4f3570
Giga <60932604+Gkhundadze@users.noreply.github.com>
2020-11-24 07:12:23 -0800

    checkbox
    added checkbox check function

--- script.js
+++ script.js
@@ -3,60 +3,62 @@
   const input = document.querySelector('input');
   const submitBtn = document.querySelector('#submit');
   const form = document.querySelector('form');
-  const listItem = document.createElement('li');
-  const listText = document.createElement('span');
-  const deleteBtn = document.createElement('button');
-  const checkbox = document.createElement('input');
   const receiveData = 'https://us-central1-js04-b4877.cloudfunctions.net/tasks';
   const addData = "https://us-central1-js04-b4877.cloudfunctions.net/tasks/create";
 
   
 checkData(receiveData);
-
-form.addEventListener("submit",(fun) => {
-  fun.preventDefault();
-  let inputValue = input.value;
+form.addEventListener("submit",(event) => {
+  event.preventDefault();
+  const inputValue = input.value;
   if(!inputValue == ""){
     createTask(inputValue);
+    
   }else{
     alert("შეიყვანეთ დასახელება")
   }
-  
 });
-
-  
-  
-  
-
-
-
-
-
-
 
 
                                 // F U N C T I O N S
 
-// receive Data from API and show in HTML
+// receive Data from API
 function checkData(url){
-  fetch(receiveData,{
+  fetch(url,{
   method: "GET"
 })
 .then(response => response.json())
 .then(data => {
-  let taskList = data.data
+  const taskList = data.data;
   taskList.forEach(task => {
     const listItem = document.createElement('li');
     const listText = document.createElement('span');
     const deleteBtn = document.createElement('button');
     const checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
-      listItem.appendChild(checkbox);
-      listItem.appendChild(listText);
-      listText.textContent = task.text;
-      listItem.appendChild(deleteBtn);
-      deleteBtn.textContent = 'Delete';
-      ul.appendChild(listItem);
+    checkbox.checked = status;
+    listItem.appendChild(checkbox);
+    listItem.appendChild(listText);
+    listText.textContent = task.text;
+    listItem.appendChild(deleteBtn);
+    deleteBtn.textContent = 'Delete';
+    ul.appendChild(listItem);
+    checkbox.onchange = ()=>{
+      if(checkbox.checked){
+        listText.style.textDecoration = "line-through";
+        listText.style.backgroundColor = "#7dc07d";
+        listText.style.letterSpacing = 4 + "px";
+        listText.style.fontWeight = "bold";
+    }else{
+      listText.style.textDecoration = "none";
+      listText.style.backgroundColor = "transparent";
+      listText.style.letterSpacing = 1 + "px";
+      listText.style.fontWeight = "normal";
+    }
+    }
+    
+
+      
       
       // STYLING
       ul.style.listStyleType = "none"
@@ -65,18 +67,45 @@ function checkData(url){
 });
 };
   // AddTask to API
-  function createTask(inputValue){
+  function createTask(text){
     fetch(addData,{
     method: "POST",
     headers: {
       'Content-Type': 'application/json',
     },
-      body: JSON.stringify({text: inputValue})
+      body: JSON.stringify({text: text})
   })
   .then(response => response.json())
   .then(data => {
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const deleteBtn = document.createElement('button');
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    listItem.appendChild(checkbox);
+      listItem.appendChild(listText);
+      listText.textContent = input.value;
+      listItem.appendChild(deleteBtn);
+      deleteBtn.textContent = 'Delete';
+      ul.appendChild(listItem);
+    checkbox.onchange = ()=>{
+      if(checkbox.checked){
+        listText.style.textDecoration = "line-through";
+        listText.style.backgroundColor = "#7dc07d";
+        listText.style.letterSpacing = 4 + "px";
+        listText.style.fontWeight = "bold";
+    }else{
+      listText.style.textDecoration = "none";
+      listText.style.backgroundColor = "transparent";
+      listText.style.letterSpacing = 1 + "px";
+      listText.style.fontWeight = "normal";
+    }
+    }
+      
+      // STYLING
+      ul.style.listStyleType = "none"
+      deleteBtn.classList.add("btn", "btn-secondary", "btn-sm")
     input.value = '';
     input.focus();
-    dataToHTM(data);
   });
 }
