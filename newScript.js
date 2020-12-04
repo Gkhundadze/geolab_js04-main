5154c32ca604a71f1128b6b30600da8c33f13d5c
Giga <60932604+Gkhundadze@users.noreply.github.com>
2020-12-04 12:38:09 +0400

    update

--- newScript.js
+++ newScript.js
@@ -2,7 +2,11 @@ const ul = document.querySelector('ul');
 const input = document.querySelector('input');
 const submitBtn = document.querySelector('#submit');
 const form = document.querySelector('form');
+
+
 const baseUrl = "https://us-central1-js04-b4877.cloudfunctions.net/tasks"
+
+
 receiveData()
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -48,41 +52,52 @@ function unCheckTask(id) {
     fetch(baseUrl + '/uncheck/' + id, {
         method: "POST"
     })
+    
 }
-function receiveData(){
+function receiveData() {
     fetch(baseUrl)
-    .then(response => response.json())
-    .then(data => {
-        const dataText = data.data
-        dataText.forEach(element => {
-            let taskCompleted = element.completed
-            const taskID = element.id
-            const listItem = document.createElement('li');
-            const listText = document.createElement('span');
-            const deleteBtn = document.createElement('button');
-            const checkbox = document.createElement('input');
-            checkbox.setAttribute('type', 'checkbox');
-            listItem.setAttribute('data-id', taskID);
-            listItem.appendChild(checkbox);
-            listItem.appendChild(listText);
-            listText.textContent = element.text;
-            listItem.appendChild(deleteBtn);
-            deleteBtn.textContent = 'Delete';
-            ul.appendChild(listItem);
-            if (element.completed == true) {
-                checkbox.setAttribute("checked", "true")
-            }
-            deleteBtn.addEventListener("click", () => {
-                deleteTask(baseUrl, taskID)
-            })
-            checkbox.addEventListener('change', () => {
+        .then(response => response.json())
+        .then(data => {
+            const dataText = data.data
+            dataText.forEach(element => {
+
+
+
+                const taskID = element.id
+                const listItem = document.createElement('li');
+                const listText = document.createElement('span');
+                const deleteBtn = document.createElement('button');
+                const checkbox = document.createElement('input');
+                checkbox.setAttribute('type', 'checkbox');
+                listItem.setAttribute('data-id', taskID);
+                listItem.appendChild(checkbox);
+                listItem.appendChild(listText);
+                listText.textContent = element.text;
+                listItem.appendChild(deleteBtn);
+                deleteBtn.textContent = 'Delete';
+                ul.appendChild(listItem);
+
+
+
+                // STYLING
+                ul.style.listStyleType = "none"
+                deleteBtn.classList.add("btn", "btn-sm", "btn-danger")
+
                 if (element.completed == true) {
-                    unCheckTask(taskID)
-                } else {
-                    checkTask(taskID)
                     checkbox.setAttribute("checked", "true")
                 }
-            })
-        });
-    })
+                deleteBtn.addEventListener("click", () => {
+                    deleteTask(baseUrl, taskID)
+                })
+                checkbox.addEventListener('change', () => {
+                    if (element.completed == true) {
+                        unCheckTask(taskID)
+                    } else {
+                        checkTask(taskID)
+                        checkbox.setAttribute("checked", "true")
+                    }
+
+                })
+            });
+        })
 }
